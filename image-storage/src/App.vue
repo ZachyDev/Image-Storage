e6c1a842cb9b6275398d8e0c093e7bc42cfe38ba
ZachyDev <mosetizachary001@gmail.com>
2020-07-15 14:21:44 +0300

    fixed an issue with the branches

---
+++ image-storage/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Hello from Zachy
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ image-storage/src/main.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import App from './App';
+
+// create a vue instance
+new Vue({
+    render: h => h(App)
+}).$mount("#app")
