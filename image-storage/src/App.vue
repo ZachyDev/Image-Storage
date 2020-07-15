787c07c742f2578af753b8b43f44fb5cf379b7ad
ZachyDev <mosetizachary001@gmail.com>
2020-07-15 15:11:53 +0300

    added the app header component

--- image-storage/src/App.vue
+++ image-storage/src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-      Hello from Zachy.
+      <AppHeader />
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader';
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      AppHeader
+    }
 }
 </script>
 
---
+++ image-storage/src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Imagy</h1>
+</template>
+
+<script>
+export default {
+    name: "AppHeader"
+}
+</script>
+
+<style>
+
+</style>
