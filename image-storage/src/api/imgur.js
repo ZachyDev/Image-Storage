b6f722f76e16dbb6eab0310effb722309bb4bfbf
ZachyDev <mosetizachary001@gmail.com>
2020-07-16 13:41:46 +0300

    forming Oauth2 URL

---
+++ image-storage/src/api/imgur.js
@@ -0,0 +1,13 @@
+import qs from 'qs';
+const CLIENT_ID = '1515796c56d5751';
+const ROOT_URL = 'https://api.imgur.com';
+
+export default {
+    login() {
+        const query_string = {
+            client_id: CLIENT_ID,
+            resonse_type: 'token'
+        }
+        `${ ROOT_URL }/oauth2/authorize?${qs.stringify(query_string)}`;
+    }
+}
