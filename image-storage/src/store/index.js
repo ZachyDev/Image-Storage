3ade9b6341b5779f694a5dbdc9ed7f11014e2c75
ZachyDev <mosetizachary001@gmail.com>
2020-07-16 12:24:14 +0300

    connected Vuex to Vue

---
+++ image-storage/src/store/index.js
@@ -0,0 +1,11 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+// connect vuex to vue
+Vue.use(Vuex);
+
+// create an instanceof the store object
+
+export default new Vuex.Store({
+    modules: {}
+});
