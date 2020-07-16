a39ef8eec732256621a983a2c573c7b048daa1c3
ZachyDev <mosetizachary001@gmail.com>
2020-07-16 14:42:34 +0300

    Initial OAuth request

--- image-storage/src/api/imgur.js
+++ image-storage/src/api/imgur.js
@@ -11,4 +11,4 @@ export default {
         window.location = `${ ROOT_URL }/oauth2/authorize?${qs.stringify(query_string)}`
     }
 }
-http://localhost:8080/oauth2/callback#access_token=51e3d4e9487496f8de675183ffde6282a1421c01&expires_in=315360000&token_type=bearer&refresh_token=45f9f56522900ec4b7676dbecf36a8a36cab2629&account_username=ZachyDev&account_id=134670835
+
