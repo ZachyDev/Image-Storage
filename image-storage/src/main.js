2850f5d82f1e089857757f854e6c36ca2fdbf126
ZachyDev <mosetizachary001@gmail.com>
2020-07-20 00:37:20 +0300

    updated the vue-router

--- image-storage/src/main.js
+++ image-storage/src/main.js
@@ -4,17 +4,20 @@ import store from './store';
 import VueRouter from 'vue-router';
 import AuthHandler from './components/AuthHandler';
 import About from './components/About';
+
 // connect vue with vue-router
 Vue.use(VueRouter);
-// create a new instance of vue router object
 
+// create an instance of the vue-router object
 const router = new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/oauth2/callback',component: AuthHandler },
+        { path: '/oauth2/callback', component: AuthHandler },
         { path: '/about',component: About }
     ]
 })
+
+
 new Vue({
     router,
     store,
